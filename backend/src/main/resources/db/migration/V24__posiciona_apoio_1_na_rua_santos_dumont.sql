72391c1bfc0b5ca64c65b00090dc0a248c883b21
Rogério de Sá <rogeriodesaf@gmail.com>
2026-07-31 19:54:34 -0300

    posiciona apoio 1 na rua Santos Dumont

---
+++ backend/src/main/resources/db/migration/V24__posiciona_apoio_1_na_rua_santos_dumont.sql
@@ -0,0 +1,18 @@
+-- Posiciona a Equipe Apoio 1 no lado da Rua Santos Dumont, em Vila Regina.
+-- A Equipe Roxa recebe a area anterior da Apoio 1 para manter a cobertura
+-- continua, sem lacunas ou sobreposicoes.
+
+UPDATE territories
+SET polygon_geojson = CASE
+      WHEN team_id IN (SELECT id FROM teams WHERE name = 'Equipe Apoio 1')
+        THEN '{"type":"Polygon","coordinates":[[[-35.060144,-6.789774],[-35.061333,-6.785316],[-35.064853,-6.785654],[-35.065928,-6.789538],[-35.060566,-6.790151],[-35.060144,-6.789774]]]}'
+      WHEN team_id IN (SELECT id FROM teams WHERE name = 'Equipe Roxa')
+        THEN '{"type":"Polygon","coordinates":[[[-35.075279,-6.784567],[-35.074709,-6.780900],[-35.079200,-6.780900],[-35.079200,-6.785010],[-35.075279,-6.784567]]]}'
+      ELSE polygon_geojson
+    END,
+    distribution_version = 'mapa-rio-2026-07-31-v3',
+    published_at = CURRENT_TIMESTAMP
+WHERE distribution_version = 'mapa-rio-2026-07-31-v2'
+  AND team_id IN (
+    SELECT id FROM teams WHERE name IN ('Equipe Apoio 1', 'Equipe Roxa')
+  );
